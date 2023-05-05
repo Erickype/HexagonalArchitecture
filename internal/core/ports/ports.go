e5842ce6fe2b54ca185bcb676a41f56d69c22f9f
Erickype <67160282+Erickype@users.noreply.github.com>
2023-05-05 16:04:05 -0500

    Service and repository interface for messages

--- internal/core/ports/ports.go
+++ internal/core/ports/ports.go
@@ -1,1 +1,15 @@
 package ports
+
+import "HexagonalArchitecture/internal/core/domain"
+
+type MessengerService interface {
+	SaveMessage(message domain.Message) error
+	ReadMessage(id string) (*domain.Message, error)
+	ReadMessages() ([]*domain.Message, error)
+}
+
+type MessengerRepository interface {
+	SaveMessage(message domain.Message) error
+	ReadMessage(id string) (*domain.Message, error)
+	ReadMessages() ([]*domain.Message, error)
+}
