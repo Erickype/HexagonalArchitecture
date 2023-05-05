70d063c83385cc0eacca12d27bd1b6a8be93536b
Erickype <67160282+Erickype@users.noreply.github.com>
2023-05-05 16:11:09 -0500

    MessengerService implemented

--- internal/core/services/services.go
+++ internal/core/services/services.go
@@ -1,1 +1,24 @@
 package services
+
+import (
+	"github.com/Erickype/HexagonalArchitecture/internal/core/domain"
+	"github.com/Erickype/HexagonalArchitecture/internal/core/ports"
+	"github.com/google/uuid"
+)
+
+type MessengerService struct {
+	repository ports.MessengerRepository
+}
+
+func (m *MessengerService) SaveMessage(message domain.Message) error {
+	message.Id = uuid.New().String()
+	return m.repository.SaveMessage(message)
+}
+
+func (m *MessengerService) ReadMessage(id string) (*domain.Message, error) {
+	return m.repository.ReadMessage(id)
+}
+
+func (m *MessengerService) ReadMessages() ([]*domain.Message, error) {
+	return m.ReadMessages()
+}
