22873fd77e5e8837356c0dc721d69705a80a2a2a
Erickype <67160282+Erickype@users.noreply.github.com>
2023-05-05 17:33:20 -0500

    ReadMessage and ReadMessages repository methods implemented

--- internal/adapters/repository/postgres.go
+++ internal/adapters/repository/postgres.go
@@ -18,3 +18,21 @@ func (m *MessengerPostgresRepository) SaveMessage(message domain.Message) error 
 	}
 	return nil
 }
+
+func (m *MessengerPostgresRepository) ReadMessage(id string) (*domain.Message, error) {
+	message := &domain.Message{}
+	request := *m.db.First(&message, "id=?", id)
+	if request.RowsAffected == 0 {
+		return nil, errors.New(fmt.Sprintf("no message found: %t", request.Error))
+	}
+	return message, nil
+}
+
+func (m *MessengerPostgresRepository) ReadMessages() ([]*domain.Message, error) {
+	var messages []*domain.Message
+	request := *m.db.Find(&messages)
+	if request.RowsAffected == 0 {
+		return nil, errors.New(fmt.Sprintf("no messages found: %t", request.Error))
+	}
+	return messages, nil
+}
