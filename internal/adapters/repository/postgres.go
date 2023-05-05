d6ead0b89b1b7151de3b478b6a1b4f71b45248f9
Erickype <67160282+Erickype@users.noreply.github.com>
2023-05-05 17:15:17 -0500

    SaveMessage repository method implemented

--- internal/adapters/repository/postgres.go
+++ internal/adapters/repository/postgres.go
@@ -1,7 +1,20 @@
 package repository
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+	"fmt"
+	"github.com/Erickype/HexagonalArchitecture/internal/core/domain"
+	"github.com/jinzhu/gorm"
+)
 
 type MessengerPostgresRepository struct {
 	db *gorm.DB
+}
+
+func (m *MessengerPostgresRepository) SaveMessage(message domain.Message) error {
+	request := m.db.Create(message)
+	if request.RowsAffected == 0 {
+		return errors.New(fmt.Sprintf("message not saved: %t", request.Error))
+	}
+	return nil
 }
