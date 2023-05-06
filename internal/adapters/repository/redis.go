87794c129c9eabc2eadfd47e6e7ae9b0e6e75494
Erickype <67160282+Erickype@users.noreply.github.com>
2023-05-06 16:22:53 -0500

    SaveMessage for redis repository implemented

--- internal/adapters/repository/redis.go
+++ internal/adapters/repository/redis.go
@@ -1,7 +1,20 @@
 package repository
 
-import "github.com/go-redis/redis/v7"
+import (
+	"encoding/json"
+	"github.com/Erickype/HexagonalArchitecture/internal/core/domain"
+	"github.com/go-redis/redis/v7"
+)
 
 type MessengerRedisRepository struct {
 	client *redis.Client
+}
+
+func (m *MessengerRedisRepository) SaveMessage(message domain.Message) error {
+	data, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+	m.client.HSet("messages", message.Id, data)
+	return nil
 }
