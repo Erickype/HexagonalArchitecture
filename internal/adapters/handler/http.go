9452cabe6c1a37b44e4bafb6218aa9fb5028126c
Erickype <67160282+Erickype@users.noreply.github.com>
2023-05-07 12:51:45 -0500

    HttpHandler struct that accepts a MessengerService

--- internal/adapters/handler/http.go
+++ internal/adapters/handler/http.go
@@ -1,1 +1,13 @@
 package handler
+
+import "github.com/Erickype/HexagonalArchitecture/internal/core/services"
+
+type HttpHandler struct {
+	service services.MessengerService
+}
+
+func NewHttpHandler(service services.MessengerService) *HttpHandler {
+	return &HttpHandler{
+		service: service,
+	}
+}
