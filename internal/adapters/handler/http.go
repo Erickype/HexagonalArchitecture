2d1f3065b777656a7449b6b8f76e8c451144d577
Erickype <67160282+Erickype@users.noreply.github.com>
2023-05-07 13:09:31 -0500

    Save message handler created

--- internal/adapters/handler/http.go
+++ internal/adapters/handler/http.go
@@ -1,9 +1,35 @@
 package handler
 
-import "github.com/Erickype/HexagonalArchitecture/internal/core/services"
+import (
+	"github.com/Erickype/HexagonalArchitecture/internal/core/domain"
+	"github.com/Erickype/HexagonalArchitecture/internal/core/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 type HttpHandler struct {
 	service services.MessengerService
+}
+
+func (h *HttpHandler) SaveMessage(ctx *gin.Context) {
+	var message domain.Message
+	err := ctx.ShouldBindJSON(&message)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	err = h.service.SaveMessage(message)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{
+		"success": "message created",
+	})
 }
 
 func NewHttpHandler(service services.MessengerService) *HttpHandler {
